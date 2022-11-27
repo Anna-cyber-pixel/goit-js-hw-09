a72129122ec11b0a740810a61a116bafeb4f8464
Anna Kravets <superpower390@gmail.com>
2022-11-27 21:11:33 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+function getBackgraundColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+let colorId = null;
+
+refs.startBtn.addEventListener('click', () => {
+  colorId = setInterval(getBackgraundColor, 1000);
+  refs.startBtn.setAttribute('disabled', true);
+  refs.stopBtn.removeAttribute('disabled');
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  clearInterval(colorId);
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', true);
+});
